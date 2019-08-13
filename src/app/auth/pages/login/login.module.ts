1c86a3aa40a4045a55bec5372373b628f3700c32
iurimega13 <iurimega13@gmail.com>
2019-08-13 20:41:15 -0300

    adicionando e configurando sharedmodule

--- src/app/auth/pages/login/login.module.ts
+++ src/app/auth/pages/login/login.module.ts
@@ -1,9 +1,7 @@
+import { SharedModule } from './../../../shared/shared.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
 
@@ -16,9 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [LoginPage]
