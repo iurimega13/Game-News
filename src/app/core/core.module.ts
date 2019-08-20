f11fb6b021c5f03e224d05d74364d701060d7c98
iurimega13 <iurimega13@gmail.com>
2019-08-20 14:49:50 -0300

    adicionando coreModule

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    IonicModule.forRoot()
+  ]
+})
+export class CoreModule { }
