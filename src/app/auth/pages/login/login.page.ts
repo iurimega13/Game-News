a496d529d6408d760ef96611a1487ee77f6d5b4d
iurimega13 <iurimega13@gmail.com>
2019-08-29 19:21:22 -0300

    adicionando parte de criar conta, só que um erro perciste

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
-
   authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Create account'
+  };
 
-  constructor(private fb: FormBuilder) { }
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.createForm();
@@ -18,20 +29,30 @@ export class LoginPage implements OnInit {
   // Validador dosformulariosdapágina de login
   private createForm(): void {
     this.authForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
-    })
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]]
+    });
   }
-
+  get nome(): FormControl {
+    return <FormControl>this.authForm.get("nome");
+  }
   get email(): FormControl {
-    return <FormControl>this.authForm.get('email')
+    return <FormControl>this.authForm.get("email");
   }
   get password(): FormControl {
-    return <FormControl>this.authForm.get('password')
+    return <FormControl>this.authForm.get("password");
+  }
+
+  chageAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Criar Conta';
+    this.configs.actionChange = isSignIn ? 'Criar Conta' : 'Conta Existente';
+    !isSignIn
+      ? this.authForm.addControl('nome', this.nameControl)
+      : this.authForm.removeControl('nome');
   }
   onSubmit(): void {
     console.log(this.authForm.value);
-    
   }
-
 }
